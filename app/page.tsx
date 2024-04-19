04d2af4048cd7930e017888e1b1d6dd5ef4db499
Kakooza Vianey <kakoozavian@gmail.com>
2024-04-20 02:00:51 +0300

    updates to the repo body

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,142 @@
-import Image from "next/image";
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
+import Link from 'next/link'
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  AwaitedReactNode,
+} from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      className="flex min-h-screen flex-col p-2
+    // bg-[url('/weare.jpg')] 
+    bg-gradient-to-r from-[#e5e7eb] to-[#f3f4f6]
+    "
+    >
+      {/* <Navbar /> */}
+
+      <div className="mt-4 flex grow flex-col md:flex-row">
+        <div className="flex flex-col justify-center rounded-lg bg-gray-100 px-2 py-2 md:w-2/5 md:px-10">
+          <p
+            className={`
+            //
+            text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <strong>E-Ticketing.</strong> A place for you{' '}
+            <Link
+              href="https://decentracode.xyz/dashboard"
+              className="text-blue-400 md:text-md"
+            >
+              to purchase tickets for web3 events,{' '}
+            </Link>
+            , Click on the link below.
+          </p>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Explore</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            className="block md:hidden"
+            src="/hero-anima.gif"
+            width={560}
+            height={620}
+            alt="For mobile"
+          />
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <section
+        id="about"
+        className="mt-2 w-full flex relative items-center justify-center p-4"
+      >
+        <div className="absolute -z-10 h-full w-full overflow-hidden">
+          <Image
+            src="/whirl.svg"
+            fill
+            className="absolute object-cover w-full overflow-visible sm:rotate-90"
+            alt="Background Whirl"
+          />
+        </div>
+        <div className="w-full h-full flex items-center justify-center flex-col gap-8 max-w-7xl">
+          <h3 className="text-3xl md:text-5xl font-bold text-slate-950">
+            Buidler Forcused!
+          </h3>
+          <div className="w-full grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1 gap-4 justify-between relative">
+            {infoCards.map((infoCard: { id: Key | null | undefined; icon: any; title: string; bodyText: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; }) => {
+            return (<div>Search.</div>)
+          </div>
+        </div>
+      </section> */}
     </main>
-  );
+  )
+}
+
+interface IInfoCardProps {
+  title: string
+  // Icon: LucideIcon
+  children: ReactElement<any, any>
+}
+
+function InfoCard({ title, children }: IInfoCardProps) {
+  return (
+    <div className="w-full h-60 flex flex-col justify-around items-center p-4 bg-gray-900 rounded bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20">
+      <div className="p-4 bg-blue-400 rounded-full">{/* <Icon /> */}</div>
+      <div>
+        <h3 className="text-md font-bold sm:text-xl">{title}</h3>
+      </div>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+function Navbar() {
+  return (
+    <div className="w-full h-16 backdrop-filter backdrop-blur-xl bg-opacity-10 border-b flex items-center justify-center">
+      <div className="max-w-7xl w-full flex items-center justify-between p-4">
+        <h6 className="font-bold">Yo Ticket</h6>
+        <ul className="flex gap-8">
+          <li>
+            <Link
+              className="hover:text-blue-400 transition-colors text-xs sm:text-base"
+              href="#home"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-blue-400 transition-colors text-xs sm:text-base"
+              href="#about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-blue-400 transition-colors text-xs sm:text-base"
+              href="#pricing"
+            >
+              Explore DAPP
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }

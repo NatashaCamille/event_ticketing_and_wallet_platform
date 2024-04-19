5dd3f4aeaec930776906896f221f60184ed09869
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-19 21:28:44 +0000

    Update Navbar component with Next.js links and Tailwind CSS styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,50 @@
-const Navbar = () => {
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
     return (
-        <nav>
-            <div className="nav-wrapper">
-                <a href="#" className="brand-logo">Logo</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto">
+                <ul className="flex flex-wrap justify-between items-center">
+                    <div className=''>
+                        {/* <img src="/logo.png" alt="logo" className="w-10 h-10" /> */}
+                        Logo
+                    </div>
+                    <div className='flex justify-between items-center'>
+                        <li className="mx-2">
+                            <Link className="text-white hover:text-gray-300" href="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="mx-2">
+                            <Link className="text-white hover:text-gray-300" href="/schedule">
+                                Schedule
+                            </Link>
+                        </li>
+                        <li className="mx-2">
+                            <Link className="text-white hover:text-gray-300" href="/speakers">
+                                Speakers
+                            </Link>
+                        </li>
+                        <li className="mx-2">
+                            <Link className="text-white hover:text-gray-300" href="/venue">
+                                Venue
+                            </Link>
+                        </li>
+                        <li className="mx-2">
+                            <Link className="text-white hover:text-gray-300" href="/register">
+                                Register
+                            </Link>
+                        </li>
+                    </div>
+                    <div className=''>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Connect wallet
+                        </button>
+                    </div>
                 </ul>
             </div>
         </nav>
     );
-}
+};
 
 export default Navbar;

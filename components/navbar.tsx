601e0549e4a37e4a86c2723cf6ed31e2ceda4f81
Kyagulanyi Allan <97457918+Allo-lala@users.noreply.github.com>
2024-04-20 03:30:49 +0300

    Revert "puuttty"

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,5 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
-import React from 'react';
 
 const Navbar: React.FC = () => {
     return (

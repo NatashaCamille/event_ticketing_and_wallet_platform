95bf907780a2b0afee0d77cc3505cb47677611a3
Natasha Camille <123318520+NatashaCamille@users.noreply.github.com>
2024-04-30 13:01:19 +0000

    Adjusting the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,7 @@ const DashboardPage = () => {
                 <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
                 <div className="bg-white shadow-md rounded-lg p-4">
                     <h2 className="text-xl font-bold mb-4">Welcome to Your Dashboard</h2>
-                    <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ac dui condimentum, sed tempor elit tincidunt. Integer vitae elit a magna interdum laoreet. Nunc pulvinar, nisi a tristique vehicula, ligula odio finibus elit, nec tincidunt magna lacus vel est. Vivamus et eleifend elit. Donec ac sagittis magna. Duis tincidunt fermentum sapien, in auctor velit malesuada sit amet. Mauris in pulvinar tortor.</p>
+                    {/* <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ac dui condimentum, sed tempor elit tincidunt. Integer vitae elit a magna interdum laoreet. Nunc pulvinar, nisi a tristique vehicula, ligula odio finibus elit, nec tincidunt magna lacus vel est. Vivamus et eleifend elit. Donec ac sagittis magna. Duis tincidunt fermentum sapien, in auctor velit malesuada sit amet. Mauris in pulvinar tortor.</p> */}
                     <p className="text-gray-600">Our event platform offers a range of features to help you manage and promote your events. You can create events, sell tickets, manage attendees, and much more. Explore the platform to discover all the tools available to you!</p>
                 </div>
                 <div className="mt-8 bg-white shadow-md rounded-lg p-4">
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,11 +6,11 @@ const Sidebar = () => {
 
         <div className='bg-black/45'>
             <div className='flex flex-col items-center gap-4 h-screen m-4'>
-                <Link href="#" className="text-white/50 hover:text-white">Home</Link>
+                {/* <Link href="#" className="text-white/50 hover:text-white">Home</Link>
                 <Link href="/create-event" className="text-white/50 hover:text-white">CREATE EVENT</Link>
                 <Link href="#" className="text-white/50 hover:text-white">Explore DAPP</Link>
                 <Link href="#" className="text-white/50 hover:text-white">Explore DAPP</Link>
-                <Link href="#" className="text-white/50 hover:text-white">Explore DAPP</Link>
+                <Link href="#" className="text-white/50 hover:text-white">Explore DAPP</Link> */}
 
         <div className='bg-gray-900 text-white w-1/5 h-full'>
             <div className='flex flex-col items-center justify-center gap-12 h-full'>

28dec9ed378c0898172f76babb4a8d6a2892b664
Natasha Camille <123318520+NatashaCamille@users.noreply.github.com>
2024-04-30 13:29:12 +0000

    Minor changes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
 
         <div className='bg-gray-900 text-white w-1/5 h-full'>
             <div className='flex flex-col items-center justify-center gap-12 h-full'>
-                <Link href="/dashboard" className="text-white hover:text-gray-300 py-2 px-4">Home</Link>
+                {/* <Link href="/dashboard" className="text-white hover:text-gray-300 py-2 px-4">Home</Link> */}
                 <Link href="/create-event-page" className="text-white hover:text-gray-300 py-2 px-4">Create Event</Link>
                 <Link href="/list-events-page" className="text-white hover:text-gray-300 py-2 px-4">List Events</Link>
             </div>
